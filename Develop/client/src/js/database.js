4a508d72591597eff91beff1263758f7f6fd173b
Avery <averygallagan@yahoo.com>
2024-07-23 20:15:14 -0400

    fixing more problems

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -17,9 +17,8 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add(content);
+  const request = store.put({ id: 1, value: content});
   await request;
-  await tx.done;
   console.log('added to database:', content);
 };
 
@@ -28,9 +27,8 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log('result.value', result);
   return result;
 };
 
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = () => {
       }),
 
       new InjectManifest({
-        swSrc: path.resolve(__dirname, 'client/src-sw.js'),
+        swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
 
